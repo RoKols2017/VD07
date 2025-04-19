543192606e7e85373e5534f02d5c22b028940af4
RoKols2017 <RoKols2017@gmail.com>
2025-04-19 16:35:56 +0300

    Refactored, now works through an application factory.

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,24 @@
+# app/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from config import Config
 
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'main.login'
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
 
-from app import routes
+    from app.routes import main
+    app.register_blueprint(main)
+
+    return app
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,21 @@
-from flask import render_template, request, redirect, url_for, flash
+# app/routes.py
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, current_user, login_required
 from app.models import User
-from app import app, db, bcrypt
+from app import db, bcrypt
 from app.forms import RegistrationForm, LoginForm
 
-@app.route('/')
-@app.route('/home')
+main = Blueprint('main', __name__)
+
+@main.route('/')
+@main.route('/home')
 def home():
     return render_template('home.html')
 
-@app.route('/register', methods=['GET', 'POST'])
+@main.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('main.home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -20,30 +23,29 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Вы успешно зарегистрировались!', 'success')
-        return redirect(url_for('login'))
+        return redirect(url_for('main.login'))
     return render_template('register.html', form=form, title='Register')
 
-@app.route('/login', methods=['GET', 'POST'])
+@main.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('main.home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            return redirect(url_for('main.home'))
         else:
             flash('Введены неверные данные')
     return render_template('login.html', form=form, title='Login')
 
-
-@app.route('/logout')
+@main.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for('main.home'))
 
-@app.route('/account')
+@main.route('/account')
 @login_required
 def account():
     return render_template('account.html')
